17301bdd0b3517edda076c9d31a462afe58361a2
NekoMimiUnagi <rxl190028@utdallas.edu>
2023-04-08 11:30:20 -0500

    Manage scripts
    1. Solve conflicts of class Ray. Change its name to RayGun.
    2. Move scripts to corresponding folders

--- Assets/Scripts/Weapons/RayGun.cs
+++ Assets/Scripts/Weapons/RayGun.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ray : MonoBehaviour
+public class RayGun : MonoBehaviour
 {
     // Start is called before the first frame update
     
