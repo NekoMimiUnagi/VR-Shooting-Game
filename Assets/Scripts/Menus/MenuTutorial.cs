013d7f2d7a68ab033a1692a46f77972a56b0021f
Yifan Lin <92171160+linyifan1024@users.noreply.github.com>
2023-05-02 22:07:11 -0500

    add tutorial

---
+++ Assets/Scripts/Menus/MenuTutorial.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTutorial : MonoBehaviour
+{
+    public GameObject tutorial;
+
+    // Start is called before the first frame update
+    public void tutorialStart()
+    {
+        tutorial.SetActive(true);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
